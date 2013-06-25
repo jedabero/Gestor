80cb520679e242942fa12a88bcc42ec89e1a81f1
jedabero <jedabero@hotmail.com>
2013-06-25 15:12:22 -0500

    Continued items

--- Gestor/src/items/Item.java
+++ Gestor/src/items/Item.java
@@ -10,16 +10,58 @@ public class Item {
 	private String nombre;
 	private long id;
 	
+	/**
+	 * @return el valor del item
+	 */
+	public double getPrecio() {
+		return precio;
+	}
+
+	/**
+	 * @param precio el nuevo valor del item
+	 */
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	/**
+	 * @return el nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre el nuevo nombre
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return el numero de identificacion
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id la nueva id
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public Item (){
-		this.precio = 0d;
-		this.nombre = "";
-		this.id = 0L;
+		this.setPrecio(0d);
+		this.setNombre("");
+		this.setId(0L);
 	}
 	
 	public Item (double precio, String nombre, long id){
-		this.precio = precio;
-		this.nombre = nombre;
-		this.id = id;
+		this.setPrecio(precio);
+		this.setNombre(nombre);
+		this.setId(id);
 	}
 	
 	
