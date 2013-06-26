3a3145e86cc6937567629aedb956d88d04aea149
jedabero <jedabero@hotmail.com>
2013-06-26 18:24:04 -0500

    Conditioned item setters and finisehd Categoria Enum

--- Gestor/src/items/Categoria.java
+++ Gestor/src/items/Categoria.java
@@ -4,13 +4,25 @@
 package items;
 
 /**
+ * Una categoria indica a que sección o tipo de producto pertenece un item.
  * @author jedabero
  *
  */
 public enum Categoria {
 	
+	/** */
 	PAN,
+	/** */
 	FRITO,
-	
+	/** */
+	BEBIDA,
+	/** */
+	POSTRE,
+	/** */
+	HOJALDRA,
+	/** */
+	OTROS,
+	/** */
+	NA
 	
 }
--- Gestor/src/items/Item.java
+++ Gestor/src/items/Item.java
@@ -9,7 +9,7 @@ public class Item {
 	private int id;
 	private double valor;
 	private String nombre;
-	private String categoria;
+	private Categoria categoria;
 	private String descripcion;
 	
 	/**
@@ -23,7 +23,9 @@ public class Item {
 	 * @param id la nueva id
 	 */
 	public void setId(int id) {
-		this.id = id;
+		
+		this.id = ((id>10000)&&(id<100000))?id:0;
+		
 	}
 	
 	/**
@@ -37,7 +39,7 @@ public class Item {
 	 * @param valor el nuevo valor del item
 	 */
 	public void setValor(double valor) {
-		this.valor = valor;
+		this.valor =(valor>=0)?valor:0;
 	}
 
 	/**
@@ -51,20 +53,20 @@ public class Item {
 	 * @param nombre el nuevo nombre
 	 */
 	public void setNombre(String nombre) {
-		this.nombre = nombre;
+		this.nombre = (nombre!=null)?nombre:"nn";
 	}
 
 	/**
 	 * @return the categoria
 	 */
-	public String getCategoria() {
+	public Categoria getCategoria() {
 		return categoria;
 	}
 
 	/**
 	 * @param categoria the categoria to set
 	 */
-	public void setCategoria(String categoria) {
+	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
 
@@ -79,24 +81,24 @@ public class Item {
 	 * @param descripcion the descripcion to set
 	 */
 	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
+		this.descripcion = (descripcion!=null)?descripcion:"N/A";
 	}
 
 	public Item (){
-		this(0, null, 0d, null, null);
+		this(0, "nn", 0d, Categoria.NA, "N/A");
 	}
 	
 	public Item (int id, String nombre, double valor){
-		this(id, nombre, valor, "N/A", "N/A");
+		this(id, nombre, valor, Categoria.NA, "N/A");
 		
 	}
 	
-	public Item (int id, String nombre, double valor, String categ, String descrip){
-		 this.setId(id);
-		 this.setValor(valor);
-		 this.setNombre(nombre);
-		 this.setCategoria(categ);
-		 this.setDescripcion(descrip);
+	public Item (int id, String nombre, double valor, Categoria categ, String descrip){
+		this.setId(id);
+		this.setValor(valor);
+		this.setNombre(nombre);
+		this.setCategoria(categ);
+		this.setDescripcion(descrip);
 	}
 	
 }
